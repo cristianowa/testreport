bf4f7ae6e57369392a857e715fbbba371edea722
Cristiano Werner Araújo <cristianowerneraraujo@gmail.com>
2018-09-17 15:29:59 -0300

    Removing unused files.

--- testreport/report2html.py
+++
@@ -1,6 +0,0 @@
-from testreport import TITLE, EXPLANATION, TIP, CODE
-
-class Report2Html(object):
-    def __init__(self, sequence, filename):
-        self.sequence = sequence
-        self.filename = filename
--- testreport/report2pdf.py
+++
@@ -1,6 +0,0 @@
-
-
-class Report2Pdf(object):
-    def __init__(self, sequence, filename):
-        self.sequence = sequence
-        self.filename = filename
